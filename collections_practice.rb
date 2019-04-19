6e06a36b4f309410f36beb1c2ea29abf48583f0a
Stella McEwen <mcewen.stella@gmail.com>
2019-04-19 16:51:21 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,11 +1,9 @@
 def begins_with_r(array)
   array.map do |word|
-    if word.all? {|word.start_with?("r")
+    if word.all? {|word.start_with?("r")}
       return true 
     else 
       return false 
     end
   end
 end
-  
-  
