6b68e852f0440d920c737c3efa43e2c1d4965546
Stella McEwen <mcewen.stella@gmail.com>
2019-04-19 17:25:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,7 +21,7 @@ end
 def first_wa(array) 
   new_array = []
   array.each do |elem|
-    if elem.is symbol
+    if elem.is_a?(symbol)
       new_array << elem.to_s
   new_array.find {|word| word.start_with?("wa")}
 end
