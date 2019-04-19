0ae06c50d9d19ca9371cc10e2875ccee448072d6
Stella McEwen <mcewen.stella@gmail.com>
2019-04-19 18:04:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -47,7 +47,7 @@ def merge_data(keys, data)
   data.collect do |array|
     array.collect do |first_names, info_hash|
   keys.collect do |key_array|
-    key_array.collect do |name, info|
+    key_array.collect do |name_label, actual_name|
       if first_names = 
 end
   
